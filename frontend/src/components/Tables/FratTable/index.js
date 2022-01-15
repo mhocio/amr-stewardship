53347cdd2452326a395dd6dbf3ef551bc66ebfe7
Radosław Dubiel <radieus@gmail.com>
2022-01-15 02:46:59 +0100

    Add sticky column in frat table and error repsonse handling in login (#44)
    * Add sticky column in frat table and error repsonse handling in login
    
    * fix

--- frontend/src/components/Tables/FratTable/index.js
+++ frontend/src/components/Tables/FratTable/index.js
@@ -8,100 +8,95 @@ import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import { styled } from "@mui/material";
 
-const StyledTableRow = styled("TableRow")({
-  root: {
-    "&:nth-of-type(odd)": {
-      backgroundColor: "white"
-    },
-    "&:nth-of-type(even)": {
-      backgroundColor: "grey"
-    }
-  }
-});
-
 export default function FratTable({ fratData }) {
   return (
-      <TableContainer component={Paper}
-        sx={{
-          width: "96vw"
+    <TableContainer
+      component={Paper}
+      sx={{
+        width: "96vw"
+      }}
+    >
+      <Table
+        ssx={{
+          width: "max-content"
         }}
+        size="small"
+        aria-label="frat table"
       >
-        <Table
-          ssx={{
-            width: "max-content"
-          }}
-          size="small"
-          aria-label="a dense table"
-        >
-          <TableHead>
-            <TableRow sx={{ background: "#D6E4FC" }}>
-              <TableCell rowSpan={3}>Szczep bakteryjny</TableCell>
-              <TableCell rowSpan={3} align="center">
-                %I
-              </TableCell>
-              {fratData?.antibiotics?.map((item, i) => {
-                return (
-                  <TableCell colSpan={2} align="center">
-                    {item}
-                  </TableCell>
-                );
-              })}
-            </TableRow>
-            <TableRow sx={{ background: "#EBF2FE" }}>
-              {fratData?.antibiotics?.map((item, i) => {
-                return (
-                  <>
-                    <TableCell align="center">%S</TableCell>
-                    <TableCell align="center">F</TableCell>
-                  </>
-                );
-              })}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {fratData?.rows?.map((row) => (
-              <TableRow
-                key={row.name}
-                sx={{
-                  "&:last-child td, &:last-child th": { border: 0 },
-                  "&:nth-of-type(even)": {
-                    background: "#EBF2FE"
-                  }
-                }}
-              >
-                <TableCell component="th" scope="row">
-                  {row[0]}
+        <TableHead>
+          <TableRow sx={{ background: "#D6E4FC" }}>
+            <TableCell sx={{ position: "sticky", left: 0, backgroundColor: "#D6E4FC" }} rowSpan={3}>
+              Szczep bakteryjny
+            </TableCell>
+            <TableCell
+              rowSpan={3}
+              align="center"
+            >
+              %I
+            </TableCell>
+            {fratData?.antibiotics?.map((item, i) => {
+              return (
+                <TableCell colSpan={2} align="center">
+                  {item}
                 </TableCell>
-                {row.slice(1).map((item, i) => {
-                  return <TableCell align="center">{item}</TableCell>;
-                })}
-              </TableRow>
-            ))}
+              );
+            })}
+          </TableRow>
+          <TableRow sx={{ background: "#EBF2FE" }}>
+            {fratData?.antibiotics?.map((item, i) => {
+              return (
+                <>
+                  <TableCell align="center">%S</TableCell>
+                  <TableCell align="center">F</TableCell>
+                </>
+              );
+            })}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {fratData?.rows?.map((row) => (
             <TableRow
+              key={row.name}
               sx={{
+                "&:last-child td, &:last-child th": { border: 0 },
                 "&:nth-of-type(even)": {
                   background: "#EBF2FE"
-                },
-                height: 45
+                }
               }}
             >
-              <TableCell>Razem</TableCell>
-              <TableCell align="center">432</TableCell>
-              <TableCell />
-              <TableCell align="center">43</TableCell>
-              <TableCell />
-              <TableCell align="center">55</TableCell>
+              <TableCell component="th" scope="row" sx={{ position: "sticky", left: 0, backgroundColor: "#D6E4FC" }}>
+                {row[0]}
+              </TableCell>
+              {row.slice(1).map((item, i) => {
+                return <TableCell align="center">{item}</TableCell>;
+              })}
             </TableRow>
-            <TableRow sx={{ background: "#D6E4FC", height: 45 }}>
-              <TableCell>F/I [%] aktywności ogólnej</TableCell>
-              <TableCell align="center">432</TableCell>
-              <TableCell />
-              <TableCell align="center">43</TableCell>
-              <TableCell />
-              <TableCell align="center">55</TableCell>
-            </TableRow>
-          </TableBody>
-        </Table>
-      </TableContainer>
+          ))}
+          <TableRow
+            sx={{
+              "&:nth-of-type(even)": {
+                background: "#EBF2FE"
+              },
+              height: 45
+            }}
+          >
+            <TableCell sx={{ position: "sticky", left: 0, backgroundColor: "#D6E4FC" }}>Razem</TableCell>
+            <TableCell align="center">432</TableCell>
+            <TableCell />
+            <TableCell align="center">43</TableCell>
+            <TableCell />
+            <TableCell align="center">55</TableCell>
+          </TableRow>
+          <TableRow sx={{ background: "#D6E4FC", height: 45 }}>
+            <TableCell sx={{ position: "sticky", left: 0, backgroundColor: "#D6E4FC" }}>F/I [%] aktywności ogólnej</TableCell>
+            <TableCell align="center">432</TableCell>
+            <TableCell />
+            <TableCell align="center">43</TableCell>
+            <TableCell />
+            <TableCell align="center">55</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 }
--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,66 +1,84 @@
-import * as React from 'react';
-import { useNavigate } from 'react-router-dom';
+import * as React from "react";
+import { useNavigate } from "react-router-dom";
 
 // material-ui
-import { Paper, Button, Typography, Grid } from '@mui/material';
-import { styled } from '@mui/material/styles';
-import PersonIcon from '@mui/icons-material/Person';
-import VpnKeyIcon from '@mui/icons-material/VpnKey';
-import LoginIcon from '@mui/icons-material/Login';
+import { Paper, Button, Typography, Grid } from "@mui/material";
+import { styled } from "@mui/material/styles";
+import PersonIcon from "@mui/icons-material/Person";
+import VpnKeyIcon from "@mui/icons-material/VpnKey";
+import LoginIcon from "@mui/icons-material/Login";
 
 // project imports
-import TextfieldWrapper from '../components/Forms/Textfield';
-import LoginButton from '../components/Forms/Button';
-import BASE_URL from '../constants/BASE_URL';
+import TextfieldWrapper from "../components/Forms/Textfield";
+import LoginButton from "../components/Forms/Button";
+import BASE_URL from "../constants/BASE_URL";
 import { DrawerHeader } from "../styledComponents/StyledDrawerHeader";
 import { useLoading } from "../loading/loading-context";
+import { useSnackbar } from "notistack";
 
 // third-party
-import * as Yup from 'yup';
-import { Formik, Form } from 'formik';
-import axios from 'axios';
+import * as Yup from "yup";
+import { Formik, Form } from "formik";
+import axios from "axios";
 
 export default function LoginTab() {
-
   const { loading, setLoading } = useLoading();
   const nagivate = useNavigate();
+  const { enqueueSnackbar, closeSnackbar } = useSnackbar();
 
   return (
     <>
       <DrawerHeader />
       <Formik
-        initialValues={{ email: '', password: '' }}
+        initialValues={{ email: "", password: "" }}
         validationSchema={Yup.object({
-          username: Yup.string('Niepoprawna nazwa użytkownika')
-            .required('Wymagane'),
-          password: Yup.string()
-            .required('Wymagane')
+          username: Yup.string("Niepoprawna nazwa użytkownika").required(
+            "Wymagane"
+          ),
+          password: Yup.string().required("Wymagane")
         })}
-        onSubmit={values => {
+        onSubmit={(values) => {
           setLoading(true);
-          axios.post(`${BASE_URL}/auth/login`, {
-            "username": values.username,
-            "password": values.password
-          }
-          )
-            .then(res => {
+          axios
+            .post(`${BASE_URL}/auth/login`, {
+              username: values.username,
+              password: values.password
+            })
+            .then((res) => {
               if (res.data.authenticationToken) {
                 localStorage.setItem("user", JSON.stringify(res.data));
               }
               setLoading(false);
               window.location.reload();
             })
+            .name.catch(function (error) {
+              setLoading(false);
+              enqueueSnackbar(error.response.data, {
+                variant: "error"
+              });
+            });
         }}
       >
         <Form>
-          <Grid container spacing={4} alignItems="center" justifyContent="center">
-            <Paper style={{
-              position: 'absolute', left: '50%', top: '50%',
-              transform: 'translate(-50%, -50%)',
-              padding: '30px'
-            }}>
+          <Grid
+            container
+            spacing={4}
+            alignItems="center"
+            justifyContent="center"
+          >
+            <Paper
+              style={{
+                position: "absolute",
+                left: "50%",
+                top: "50%",
+                transform: "translate(-50%, -50%)",
+                padding: "30px"
+              }}
+            >
               <Grid item>
-                <Typography variant="h6" sx={{ paddingBottom: "15px" }}>Logowanie</Typography>
+                <Typography variant="h6" sx={{ paddingBottom: "15px" }}>
+                  Logowanie
+                </Typography>
               </Grid>
               <Grid container item spacing={2} alignItems="center">
                 <Grid item>
@@ -68,7 +86,13 @@ export default function LoginTab() {
                 </Grid>
                 <Grid item>
                   <TextfieldWrapper
-                    id="username" name="username" label="Nazwa użytkownika" type="text" fullWidth autoFocus />
+                    id="username"
+                    name="username"
+                    label="Nazwa użytkownika"
+                    type="text"
+                    fullWidth
+                    autoFocus
+                  />
                 </Grid>
               </Grid>
               <Grid container item spacing={2} alignItems="center">
@@ -77,18 +101,45 @@ export default function LoginTab() {
                 </Grid>
                 <Grid item>
                   <TextfieldWrapper
-                    id="password" name="password" label="Hasło" type="password" fullWidth />
+                    id="password"
+                    name="password"
+                    label="Hasło"
+                    type="password"
+                    fullWidth
+                  />
                 </Grid>
               </Grid>
-              <Grid container direction="column" alignItems="center" justifyContent="center">
+              <Grid
+                container
+                direction="column"
+                alignItems="center"
+                justifyContent="center"
+              >
                 <Grid item>
-                  <LoginButton type="submit" icon={<LoginIcon />}>Zaloguj</LoginButton>
+                  <LoginButton type="submit" icon={<LoginIcon />}>
+                    Zaloguj
+                  </LoginButton>
                 </Grid>
-                <Grid item sx={{marginTop: "15px"}}>
-                  <Button disableFocusRipple disableRipple variant="text" color="primary" onClick={() => nagivate('/auth/register')}>Nie mam konta</Button>
+                <Grid item sx={{ marginTop: "15px" }}>
+                  <Button
+                    disableFocusRipple
+                    disableRipple
+                    variant="text"
+                    color="primary"
+                    onClick={() => nagivate("/auth/register")}
+                  >
+                    Nie mam konta
+                  </Button>
                 </Grid>
                 <Grid item>
-                  <Button disableFocusRipple disableRipple variant="text" color="primary">Nie pamiętam hasła</Button>
+                  <Button
+                    disableFocusRipple
+                    disableRipple
+                    variant="text"
+                    color="primary"
+                  >
+                    Nie pamiętam hasła
+                  </Button>
                 </Grid>
               </Grid>
             </Paper>
