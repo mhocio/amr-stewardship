39eccb4c0d8c8831f4cd420778ec3c8f86df42e5
Radosław Dubiel <radieus@gmail.com>
2022-01-15 01:02:44 +0100

    Change frat table width (#42)

--- frontend/src/components/Tables/FratTable/index.js
+++ frontend/src/components/Tables/FratTable/index.js
@@ -20,83 +20,88 @@ const StyledTableRow = styled("TableRow")({
 });
 
 export default function FratTable({ fratData }) {
-
   return (
-    <TableContainer component={Paper}>
-      <Table
-        sx={{ minWidth: 650, overflowX: "auto" }}
-        size="small"
-        aria-label="a dense table"
+      <TableContainer component={Paper}
+        sx={{
+          width: "96vw"
+        }}
       >
-        <TableHead>
-          <TableRow sx={{ background: "#D6E4FC" }}>
-            <TableCell rowSpan={3}>Szczep bakteryjny</TableCell>
-            <TableCell rowSpan={3} align="center">
-              %I
-            </TableCell>
-            {fratData?.antibiotics?.map((item, i) => {
-              return (
-                <TableCell colSpan={2} align="center">
-                  {item}
-                </TableCell>
-              );
-            })}
-          </TableRow>
-          <TableRow sx={{ background: "#EBF2FE" }}>
-            {fratData?.antibiotics?.map((item, i) => {
-              return (
-                <>
-                  <TableCell align="center">%S</TableCell>
-                  <TableCell align="center">F</TableCell>
-                </>
-              );
-            })}
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {fratData?.rows?.map((row) => (
-            <TableRow
-              key={row.name}
-              sx={{
-                "&:last-child td, &:last-child th": { border: 0 },
-                "&:nth-of-type(even)": {
-                  background: "#EBF2FE"
-                }
-              }}
-            >
-              <TableCell component="th" scope="row">
-                {row[0]}
+        <Table
+          ssx={{
+            width: "max-content"
+          }}
+          size="small"
+          aria-label="a dense table"
+        >
+          <TableHead>
+            <TableRow sx={{ background: "#D6E4FC" }}>
+              <TableCell rowSpan={3}>Szczep bakteryjny</TableCell>
+              <TableCell rowSpan={3} align="center">
+                %I
               </TableCell>
-              {row.slice(1).map((item, i) => {
-                return <TableCell align="center">{item}</TableCell>;
+              {fratData?.antibiotics?.map((item, i) => {
+                return (
+                  <TableCell colSpan={2} align="center">
+                    {item}
+                  </TableCell>
+                );
               })}
             </TableRow>
-          ))}
-          <TableRow
-            sx={{
-              "&:nth-of-type(even)": {
-                background: "#EBF2FE"
-              },
-              height: 45
-            }}
-          >
-            <TableCell>Razem</TableCell>
-            <TableCell align="center">432</TableCell>
-            <TableCell />
-            <TableCell align="center">43</TableCell>
-            <TableCell />
-            <TableCell align="center">55</TableCell>
-          </TableRow>
-          <TableRow sx={{ background: "#D6E4FC", height: 45 }}>
-            <TableCell>F/I [%] aktywności ogólnej</TableCell>
-            <TableCell align="center">432</TableCell>
-            <TableCell />
-            <TableCell align="center">43</TableCell>
-            <TableCell />
-            <TableCell align="center">55</TableCell>
-          </TableRow>
-        </TableBody>
-      </Table>
-    </TableContainer>
+            <TableRow sx={{ background: "#EBF2FE" }}>
+              {fratData?.antibiotics?.map((item, i) => {
+                return (
+                  <>
+                    <TableCell align="center">%S</TableCell>
+                    <TableCell align="center">F</TableCell>
+                  </>
+                );
+              })}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {fratData?.rows?.map((row) => (
+              <TableRow
+                key={row.name}
+                sx={{
+                  "&:last-child td, &:last-child th": { border: 0 },
+                  "&:nth-of-type(even)": {
+                    background: "#EBF2FE"
+                  }
+                }}
+              >
+                <TableCell component="th" scope="row">
+                  {row[0]}
+                </TableCell>
+                {row.slice(1).map((item, i) => {
+                  return <TableCell align="center">{item}</TableCell>;
+                })}
+              </TableRow>
+            ))}
+            <TableRow
+              sx={{
+                "&:nth-of-type(even)": {
+                  background: "#EBF2FE"
+                },
+                height: 45
+              }}
+            >
+              <TableCell>Razem</TableCell>
+              <TableCell align="center">432</TableCell>
+              <TableCell />
+              <TableCell align="center">43</TableCell>
+              <TableCell />
+              <TableCell align="center">55</TableCell>
+            </TableRow>
+            <TableRow sx={{ background: "#D6E4FC", height: 45 }}>
+              <TableCell>F/I [%] aktywności ogólnej</TableCell>
+              <TableCell align="center">432</TableCell>
+              <TableCell />
+              <TableCell align="center">43</TableCell>
+              <TableCell />
+              <TableCell align="center">55</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
   );
 }
